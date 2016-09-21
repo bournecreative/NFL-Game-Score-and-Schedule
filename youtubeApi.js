06316dd35b36d2716a8bf64c95e4e2eea646eea3
fardanoleg <fardanoleg@gmail.com>
2016-09-21 14:38:13 -0700

    youtube API completed

--- youtubeApi.js
+++ youtubeApi.js
@@ -1,27 +1,27 @@
 /**
  * Created by OF on 9/20/16.
  */
-var video_id = null;
+
 function ajaxYoutube(teams_names) {
-    var Data = {q: teams_names, maxResults: 1, type: 'video', detailLevel: 'low'};
+    var Data = {q: teams_names, maxResults: 15, type: 'video', detailLevel: 'low'};
     $.ajax({
         dataType: 'json',
         method: 'post',
         url: "http://s-apis.learningfuze.com/hackathon/youtube/search.php",
         data: Data,
         success: function (response) {
-            console.log("Success call ajaxYoutube");
-            video_id = response.video[0].id;
+            console.log("Success call ajaxYoutube", response);
+            var random = Math.floor((Math.random() * 14) + 1);
+            console.log(random);
+            var video_id = response.video[random].id;
             console.log('result', video_id);
             var url = 'https://www.youtube.com/embed/' + video_id;
-            // flickrImage = $('<img>').attr('src', url).css("margin-top", "10%");
-            // $('#optionDiv'+divNum).append(flickrImage);
+            // video_id = $('<video>').addClass("video").attr('src', url).css('width', "320", 'height', '240');
+            // $('body').append(video_id);
             var youVideo = $('iframe').attr('src', url);
             console.log(youVideo);
 
         }
     })
 }
-
-
-ajaxYoutube('eagles');
+// ajaxYoutube('nfl colts');
