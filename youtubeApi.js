4cf6ff51427a5efccfb1b6de2935383e71ac9253
ltakiguc <ltakiguc@uci.edu>
2016-09-22 11:34:42 -0700

    sad day

--- youtubeApi.js
+++ youtubeApi.js
@@ -3,14 +3,122 @@
  */
 
 
-function ajaxYoutube(teams_names) {
+function ajaxYoutube(team, homeAway, data) {
     var id_array = [];
 
+    var team_name = null;
+    console.log("your team is:"+team)
+    var nfl_twitter_handles = ["Ravens","Bengals", "Browns", "steelers", "ChicagoBears",
+        "Lions", "packers", "Vikings", "HoustonTexans", "Colts", "Jaguars", "Titans", "AtlantaFalcons",
+        "Panthers", "Saints", "TBBuccaneers", "buffalobills", "MiamiDolphins", "nyjets", "dallascowboys",
+        "Giants", "Eagles", "Redskins", "Broncos", "Chiefs", "Chargers", "AZCardinals", "RamsNFL", "49ers", "Seahawks",
+        "Patriots","RAIDERS"];
+
+    switch (team){
+        case "BAL":
+            team_name = nfl_twitter_handles[0];
+            break;
+        case "CIN":
+            team_name = nfl_twitter_handles[1];
+            break;
+        case "CLE":
+            team_name = nfl_twitter_handles[2];
+            break;
+        case "PIT":
+            team_name = nfl_twitter_handles[3];
+            break;
+        case "CHI":
+            team_name = nfl_twitter_handles[4];
+            break;
+        case "DET":
+            team_name = nfl_twitter_handles[5];
+            break;
+        case "GB":
+            team_name = nfl_twitter_handles[6];
+            break;
+        case "MIN":
+            team_name = nfl_twitter_handles[7];
+            break;
+        case "HOU":
+            team_name = nfl_twitter_handles[8];
+            break;
+        case "IND":
+            team_name = nfl_twitter_handles[9];
+            break;
+        case "JAX":
+            team_name = nfl_twitter_handles[10];
+            break;
+        case "TEN":
+            team_name = nfl_twitter_handles[11];
+            break;
+        case "ALT":
+            team_name = nfl_twitter_handles[12];
+            break;
+        case "CAR":
+            team_name = nfl_twitter_handles[13];
+            break;
+        case "NO":
+            team_name = nfl_twitter_handles[14];
+            break;
+        case "TB":
+            team_name = nfl_twitter_handles[15];
+            break;
+        case "BUF":
+            team_name = nfl_twitter_handles[16];
+            break;
+        case "MIA":
+            team_name = nfl_twitter_handles[17];
+            break;
+        case "NYJ":
+            team_name = nfl_twitter_handles[18];
+            break;
+        case "DAL":
+            team_name = nfl_twitter_handles[19];
+            break;
+        case "NYG":
+            team_name = nfl_twitter_handles[20];
+            break;
+        case "PHI":
+            team_name = nfl_twitter_handles[21];
+            break;
+        case "WAS":
+            team_name = nfl_twitter_handles[22];
+            break;
+        case "DEN":
+            team_name = nfl_twitter_handles[23];
+            break;
+        case "KC":
+            team_name = nfl_twitter_handles[24];
+            break;
+        case "SD":
+            team_name = nfl_twitter_handles[25];
+            break;
+        case "ARI":
+            team_name = nfl_twitter_handles[26];
+            break;
+        case "LA":
+            team_name = nfl_twitter_handles[27];
+            break;
+        case "SF":
+            team_name = nfl_twitter_handles[28];
+            break;
+        case "SEA":
+            team_name = nfl_twitter_handles[29];
+            break;
+        case "NE":
+            team_name = nfl_twitter_handles[30];
+            break;
+        case "OAK":
+            team_name = nfl_twitter_handles[31];
+            break;
+        default:
+            alert("An issue occurred");
+    }
 
 
 
 
-    var Data = {q: teams_names, maxResults: 50, type: 'video', detailLevel: 'verbose'};
+        var Data = {q: team_name, maxResults: 1, type: 'video', detailLevel: 'verbose'};
     $.ajax({
         dataType: 'json',
         method: 'post',
@@ -39,12 +147,15 @@ function ajaxYoutube(teams_names) {
             //console.log('result', video_id);
             var url = 'https://www.youtube.com/embed/' + video_id;
             console.log("url: ", url);
-            video_id = $('<iframe>').attr('src', url).css('width', "320", 'height', '240');
-            $('body').append(video_id);
+            //video_id = $('<iframe>').attr('src', url).css('width', "100%", 'height', '100%');
+            /*$('body').append(video_id);*/
             var youVideo = $('iframe').attr('src', url);
             //console.log(youVideo);
-
+            if(homeAway == 1){
+                $("#1vid" + data).attr('src', url).css('width', "100%", 'height', '100%');
+            }else{
+                $("#2vid" + data).attr('src', url).css('width', "100%", 'height', '100%');
+            }
         }
     })
 }
-ajaxYoutube('philadelphia eagles');
